7c6aeb925ced93c1208d77c1c8353ea3e551e3f5
reginaldorodrigues <reginaldo.vasconcellos@hotmail.com>
2017-02-21 23:43:32 +0000

    quick fix

--- Checkout.ApiClient.Net45/Helpers/AppSettings.cs
+++ Checkout.ApiClient.Net45/Helpers/AppSettings.cs
@@ -18,7 +18,7 @@ namespace Checkout
         private static int? _requestTimeout;
         private static bool? _debugMode;
         private const string _liveUrl = "https://api2.checkout.com/v2";
-        private const string _sandboxurl = "https://sandbox.checkout.com/api";
+        private const string _sandboxUrl = "https://sandbox.checkout.com/api";
         //private const string _sandboxUrl = "http://localhost:58775/api";
         public const string ClientUserAgentName = "Checkout-DotNetLibraryClient/v1.0";
         public const string DefaultContentType = "application/json";
